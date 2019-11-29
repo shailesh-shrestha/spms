35e0883f637e18cf08556d552479c2c6ae376ce0
shailesh-shrestha <shaileshrestha@outlook.com>
2019-11-29 14:56:50 -0700

    Updates

---
+++ precache-manifest.f358013033fecf0837d1a9e39fdf2dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f2039575905aef80cbb1eec78af439",
+    "url": "/spms/index.html"
+  },
+  {
+    "revision": "737fbeabd7d59a32a060",
+    "url": "/spms/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b52a89421cf2bbf0332e",
+    "url": "/spms/static/js/2.22bd5c64.chunk.js"
+  },
+  {
+    "revision": "737fbeabd7d59a32a060",
+    "url": "/spms/static/js/main.109bc2b7.chunk.js"
+  },
+  {
+    "revision": "7cce7af0270533aefb6c",
+    "url": "/spms/static/js/runtime-main.d861ef61.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/spms/static/media/logo.25bf045c.svg"
+  }
+]);
