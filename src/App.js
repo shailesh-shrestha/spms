a3ddc3caf4275a89d1f9573a639eed922d722c08
shailesh-shrestha <shaileshrestha@outlook.com>
2019-12-10 18:38:38 -0700

    Local Storage

--- src/App.js
+++ src/App.js
@@ -6,50 +6,88 @@ import Portfolios from "./components/Portfolios";
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      portfolios: [], // List of portfolios
-      portfolioMax: false, // Will be true when maximum number of portfolio is reached.
-    };
-  }
-
-  checkPortfolioMax = () => {
-    if (this.state.portfolios.length > 9){
-      console.log(this.state.portfolios.length);
-      //  Change the state to true
-      this.setState({portfolioMax: true});
-      return true;
-    } else{ // Change the state to false
-      this.setState({portfolioMax: false});
-      return false;
+    constructor(props) {
+        super(props);
+        this.state = {
+            portfolios: [], // List of portfolios
+            portfolioMax: false, // Will be true when maximum number of portfolio is reached.
+        };
     }
-  };
 
-  handlePortfolioCreate = (title) =>
-    //This function adds a portfolio to the state
-    //If the maximum portfolios have reached alert the user
-    {
-      if (title){
-        //Add portfolio to state.
-        this.setState({portfolios: [
-            // Spread the previous state
-            ...this.state.portfolios,
-                {
-                    id: this.state.portfolios.length + 1,
-                    title: title,
-                }]});
-      }
+    // invoked immediately after a component is mounted (inserted into the tree)
+    // https://reactjs.org/docs/react-component.html#componentdidmount
+    componentDidMount() {
+        // Update Local Storage
+        this.updateLocalStorage(this.state.portfolios);
+    }
+
+    // Update local storage and current state
+    // Check if there is data in local storage
+    // Pull the data to current state if there is
+    // check whether the 'portfolio' data item is stored in web Storage
+    updateLocalStorage = (portfolios) => {
+        const portfoliosJSON = JSON.stringify(portfolios);
+        // If the local storage has portfolios
+        if (localStorage["portfolios"] ) {
+            const portfoliosLOCAL = localStorage["portfolios"];
+            // If the current state is empty and local has some data
+            if (portfolios.length === 0) {
+                const portfolios = JSON.parse(portfoliosLOCAL);
+                if (portfolios !== "undefined") {
+                    this.setState({
+                        portfolios: portfolios,
+                    });
+                }
+            } else if (portfoliosLOCAL !== portfoliosJSON) {
+                // They are different change the local state to current state
+                localStorage.clear();
+                localStorage.setItem("portfolios", portfoliosJSON);
+            }
+        } else {
+            localStorage.setItem("portfolios", portfoliosJSON);
+        }
     };
 
-  render() {
-    const portfolios = this.state.portfolios;
-    return (
-      <Paper>
-        <Typography variant="h2" align="center"> Portfolios </Typography>
-        <Portfolios portfolios={portfolios}/>
-        <Create onCreate={this.handlePortfolioCreate} portfolioMax={this.checkPortfolioMax} />
-      </Paper>
-    );
-  }
+    // This function checks the number of portfolios in current state
+    //  Returns true or false according to maximum number of Portfolio met (10)
+    checkPortfolioMax = () => {
+        if (this.state.portfolios.length > 9) {
+            //  Change the state to true
+            this.setState({portfolioMax: true});
+            return true;
+        } else { // Change the state to false
+            this.setState({portfolioMax: false});
+            return false;
+        }
+    };
+
+    //This function adds a portfolio to the state
+    // If the maximum portfolios have reached alert the user
+    handlePortfolioCreate = (title) => {
+        if (title) {
+            //Add portfolio to state.
+            let portfolios = [
+                    ...this.state.portfolios, // Spread the previous state
+                    {
+                        id: this.state.portfolios.length + 1,
+                        title: title,
+                    }];
+            this.setState({
+                portfolios: portfolios
+            });
+            // Update the local Storage with
+            this.updateLocalStorage(portfolios);
+        }
+    };
+
+    render() {
+        const portfolios = this.state.portfolios;
+        return (
+            <Paper>
+                <Typography variant="h2" align="center"> Portfolios </Typography>
+                <Portfolios portfolios={portfolios}/>
+                <Create onCreate={this.handlePortfolioCreate} portfolioMax={this.checkPortfolioMax}/>
+            </Paper>
+        );
+    }
 }
--- src/components/dialogs/Create.js
+++ src/components/dialogs/Create.js
@@ -1,111 +1,116 @@
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 
 import {
-  Fab,
-  Button,
-  TextField,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle
+    Fab,
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
 } from "@material-ui/core";
 
 import AddIcon from "@material-ui/icons/Add";
 
 export default class Create extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Open and close state of Dialogue
-      open: false,
-      // Change the state if portfolios objects changes
-      title: "",
-      // Change the button to disabled
-      disable: true,
-      // Flag for maximum number of portfolio reached.
-      maxReached: false,
-      //  Change message of ghe dialog box
-      message: "You can create an empty portfolio with portfolio name."
-    };
-  }
-  // Changes the state value property during input
-  handleChange = ({ target: { value } }) => this.setState({ title: value , disable : false });
-
-  // Toggle the state of the Dialog with form
-  handleToggle = () => this.setState({open: !this.state.open});
-
-  // Toggle the state of the Dialog when closed
-  handleClose = () => {this.setState({open: false, title: "" });};
-
-  // Toggle the state of the Message in Dialog box
-  handleMessage = (message) => {this.setState({message: message});};
-
-  // Handle the create button and send the title to App
-  handleCreate = () => {
-    const title = this.state.title;
-    const max = this.props.portfolioMax();
-    if (title.trim() === ""){
-      const m = "You need to specify a name to create a new Portfolio.";
-      this.setState({title: "", disable: true}) ;
-      this.handleMessage(m);
-    } else if (max){
-      this.setState({maxReached: true, title:"", disable: true});
-      const m = "You have reached maximum number of Portfolio allowed.";
-      this.handleMessage(m);
-    } else{
-      this.props.onCreate(title);
-      const m = "Your Portfolio was created. You can add more if you like.";
-      // Change the state of title to empty after adding.
-      this.setState({title: "", disable: true}) ;
-      this.handleMessage(m);
+    constructor(props) {
+        super(props);
+        this.state = {
+            // Open and close state of Dialogue
+            open: false,
+            // Change the state if portfolios objects changes
+            title: "",
+            // Change the button to disabled
+            disable: true,
+            // Flag for maximum number of portfolio reached.
+            maxReached: false,
+            //  Change message of ghe dialog box
+            message: "You can create an empty portfolio with portfolio name."
+        };
     }
-  };
 
-  render() {
-    const { open, title, disable, message, maxReached } = this.state;
-    return (
-      <Fragment>
-        <Fab color="primary" aria-label="add" onClick={this.handleToggle} disabled={maxReached}>
-          <AddIcon />
-        </Fab>
+    // Changes the state value property during input
+    handleChange = ({target: {value}}) => this.setState({title: value, disable: false});
 
-        <Dialog
-          open={open}
-          onClose={this.handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Create Portfolio</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              {message}
-            </DialogContentText>
-            <TextField
-              required
-              label="Enter a name."
-              value={title}
-              variant="outlined"
-              onChange={this.handleChange}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button
-              variant="contained"
-              color="Secondary"
-              onClick={this.handleClose}
-            >
-              Cancel
-            </Button><Button
-              variant="contained"
-              color="primary"
-              onClick={this.handleCreate}
-              disabled={disable}
-            >
-              Create
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Fragment>
-    );
-  }
+    // Toggle the state of the Dialog with form
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    // Toggle the state of the Dialog when closed
+    handleClose = () => {
+        this.setState({open: false, title: ""});
+    };
+
+    // Toggle the state of the Message in Dialog box
+    handleMessage = (message) => {
+        this.setState({message: message});
+    };
+
+    // Handle the create button and send the title to App
+    handleCreate = () => {
+        const title = this.state.title;
+        const max = this.props.portfolioMax();
+        if (title.trim() === "") {
+            const m = "You need to specify a name to create a new Portfolio.";
+            this.setState({title: "", disable: true});
+            this.handleMessage(m);
+        } else if (max) {
+            this.setState({maxReached: true, title: "", disable: true});
+            const m = "You have reached maximum number of Portfolio allowed.";
+            this.handleMessage(m);
+        } else {
+            this.props.onCreate(title);
+            const m = "Your Portfolio was created. You can add more if you like.";
+            // Change the state of title to empty after adding.
+            this.setState({title: "", disable: true});
+            this.handleMessage(m);
+        }
+    };
+
+    render() {
+        const {open, title, disable, message, maxReached} = this.state;
+        return (
+            <Fragment>
+                <Fab color="primary" aria-label="add" onClick={this.handleToggle} disabled={maxReached}>
+                    <AddIcon/>
+                </Fab>
+
+                <Dialog
+                    open={open}
+                    onClose={this.handleClose}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">Create Portfolio</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            {message}
+                        </DialogContentText>
+                        <TextField
+                            required
+                            label="Enter a name."
+                            value={title}
+                            variant="outlined"
+                            onChange={this.handleChange}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            variant="contained"
+                            color="Secondary"
+                            onClick={this.handleClose}
+                        >
+                            Cancel
+                        </Button><Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleCreate}
+                        disabled={disable}
+                    >
+                        Create
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </Fragment>
+        );
+    }
 }
